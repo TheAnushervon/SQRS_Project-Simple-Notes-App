f13e91f3e45e7974648d707eb407bc50b94a5624
muhammaduss <muhammad20040130@gmail.com>
2025-05-06 12:35:46 +0300

    ci: fix database url

--- simple_notes_app/app/core/database.py
+++ simple_notes_app/app/core/database.py
@@ -6,7 +6,6 @@ import os
 
 
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///app/db/notes.db")
-print(DATABASE_URL)
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
